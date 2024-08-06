2e6fa45fc9e31b53d474408e533e55eafda138d3
Hayman04 <haymanhninaye@gmail.com>
2024-08-06 21:14:29 +0800

    navbar modified

--- CP3407 Project/resources/views/includes/navbar.blade.php
+++ CP3407 Project/resources/views/includes/navbar.blade.php
@@ -3,23 +3,7 @@
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <img src="/images/Logo.png" class="h-12 w-12 object-cover" alt="Flowbite Logo">
             <div class="flex md:order-2 space-x-3 rtl:space-x-reverse">
-                <form class="max-w-md mx-auto">
-                    <div class="relative">
-                        <input type="search"
-                            class="block p-2.5 w-9/12 z-20 text-sm text-gray-900 bg-gray-50 rounded-s-lg border-s-gray-50 border-s-2 border border-gray-300 outline-none"
-                            placeholder="Search" required />
-                        <button type="submit"
-                            class="absolute top-0 end-0 p-2.5 mr-2 text-sm font-medium h-full text-white bg-gray-500 rounded-e-lg border border-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-500">
-                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                            <span class="sr-only">Search</span>
-                        </button>
-                    </div>
-                </form>
-                <div class="flex items-center space-x-4">
+                <div class="flex items-center space-x-4 justify-between w-[200px]">
                     <a href="#" class="text-white hover:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor" class="w-6 h-6" aria-hidden="true">
@@ -27,7 +11,6 @@
                                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                         </svg>
                     </a>
-
                     <a href="/cart" class="relative text-white hover:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor" class="w-6 h-6" aria-hidden="true">
